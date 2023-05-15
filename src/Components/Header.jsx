157e3981e0cc640886cb8ff12c95c627a48a2ecd
ZweiCrow <squall.flamme@gmail.com>
2023-05-15 14:49:43 +0200

    first commit of header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../Utils/Sass/header.scss"
+
+const Header = () => {
+  return (
+    <header>
+      <div id='Header'></div>
+      <div id='HeaderCarousel'>
+        <img src="./Images/Carousel1.png" alt="Carousel1" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;

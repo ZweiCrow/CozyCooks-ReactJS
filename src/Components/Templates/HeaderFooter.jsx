57bee8c38658edf9f22e4ac2a9fc81f562c5668e
ZweiCrow <squall.flamme@gmail.com>
2023-05-17 09:30:18 +0200

    Header component finished ?

--- src/Components/Templates/HeaderFooter.jsx
+++ src/Components/Templates/HeaderFooter.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer';
 
 const HeaderFooter = () => {
   return (
   <>
     <Header/>
     <Outlet/>
-      
+    <Footer/>  
   </>
   );
 };
